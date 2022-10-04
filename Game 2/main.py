1737ff09e7e515e4ada14ec2bde71970884090df
ishan <ishanluhani@gmail.com>
2022-10-04 11:14:53 +0530

    update

--- Game 2/main.py
+++ Game 2/main.py
@@ -9,11 +9,11 @@ circles = []
 while True:
     _, image = cap.read()
     image = cv2.flip(image, 1)
-    image[0:180, 0:180] = cv2.imread('tile002.png')
     hand = detector.findHands(image, flipType=True)
     if hand[0] and detector.fingersUp(hand[0][0])[1]:
         index = hand[0][0]['lmList'][8]
         circles.append(index)
+    image[180:360, 180:360] = cv2.imread('tile002.png')
     for i in circles:
         image = cv2.circle(image, i, 5, (255, 0, 0))
     cv2.imshow('image', image)
